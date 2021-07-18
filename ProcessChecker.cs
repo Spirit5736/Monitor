3fce473c654a1962b9298842072f6e6915712dbf
Кожевников Иван <kozhevnikovi@mail.ru>
2021-07-19 01:07:15 +0700

    добавлен класс проверки и завершения работы процесса

---
+++ ProcessChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Monitor
+{
+    /// <summary>Класс определяет метод расчета жизни процесса</summary>    
+    public class ProcessChecker
+    {
+        public Process Process { get; }
+
+        public int AcceptableLifetime { get; }
+
+        public int FrequencyOfVerification { get; }
+
+        public bool ProcessIsKilled { get; private set; }
+
+        /// <summary>Конструктор класса ProcessChecker</summary>
+        ///  <param name="process">экземпляр Process</param>
+        ///  <param name="acceptableLifetime">Допустимое время жизни процесса (в минутах)</param>
+        ///  <param name="frequencyOfVerification">Частота мониторинга процесса (в минутах)</param>
+        public ProcessChecker(Process process, int acceptableLifetime, int frequencyOfVerification)
+        {
+            Process = process;
+            AcceptableLifetime = acceptableLifetime;
+            FrequencyOfVerification = frequencyOfVerification;
+        }
+
+        /// <summary>Метод производит проверку жизни процесса и "убивает" процесс</summary>    
+        public void Check()
+        {
+            var delta = DateTime.Now - Process.StartTime;
+            if (delta.TotalMilliseconds >= AcceptableLifetime * 60000)
+            {
+                Console.WriteLine($"{Process.ProcessName} был завершен - {DateTime.Now}");
+                Process.Kill();
+                ProcessIsKilled = true;
+            }
+        }
+    }
+}
