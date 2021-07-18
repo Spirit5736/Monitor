68497e2176d79377b92b0f247d80b5f33fe8e334
Кожевников Иван <kozhevnikovi@mail.ru>
2021-07-18 13:27:33 +0700

    Добавлен класс program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine($"parameter count = {args.Length}");
+
+            //for (int i = 0; i < args.Length; i++)
+            //{
+            //    Console.WriteLine($"Arg[{i}] = [{args[i]}]");
+            //}
+            string name = args[0];
+            int acceptableLifetime = Convert.ToInt32(args[1]);         
+            int frequencyOfVerification = Convert.ToInt32(args[2]);
+            Console.WriteLine("идет мониторинг процесса...");
+            MonitoringOfProcess monitor = new();
+            monitor.KillProcess(name, acceptableLifetime, frequencyOfVerification);
+        }
+    }
+}
